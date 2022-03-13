225357ee14b4da96faf68cc472f0742662b6052b
Michelle Caraballo <caraballomp@yahoo.com>
2022-03-13 15:29:03 -0700

    commit tree 57a02a2a7b767cb3d80a6a123f16e4f1e088c5d3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+function domContentLoaded() {
+    document.addEventListener("DOMContentLoaded", function(e) {
+      updateDOM()
+    });
+  }
+  document.addEventListener( "DOMContentLoaded", function () {
+    updateDOM()
+  } );
+  
+  
+  function updateDOM() {
+    document.getElementById("text").innerHTML = "This is really cool!";
+    document.getElementById( "text" )
+      .innerHTML = "This is really cool!";
+  }
